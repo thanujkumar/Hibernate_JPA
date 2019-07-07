7b8c11c5b7b58db9215039335b00286251b158c0
THANUJ KUMAR S C <thanuj_tk@yahoo.com>
2019-07-07 23:40:37 +0530

    JPQL examples

---
+++ playground/src/main/java/playground/main/MainJPA_Region_JPQL.java
@@ -0,0 +1,78 @@
+package playground.main;
+
+import playground.model.Country;
+import playground.model.Location;
+import playground.model.Region;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.*;
+
+//https://thoughts-on-java.org/jpql/
+
+/**
+ * JOINs of unrelated entities are not supported by the JPA specification, but you can use a theta join which creates
+ * a cartesian product and restricts it in the WHERE clause to the records with matching foreign and primary keys.
+ * <p>
+ * https://thoughts-on-java.org/how-to-join-unrelated-entities/
+ */
+public class MainJPA_Region_JPQL {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityMgrFactory = Persistence.createEntityManagerFactory("PLAYGROUND");
+        EntityManager entityMgr = entityMgrFactory.createEntityManager();
+
+        Query query = entityMgr.createQuery("select R  from Region R");
+        List<Region> regionList = query.getResultList();
+        for (Region r : regionList) {
+            r.getCountries().forEach(x -> {
+                System.out.println("Region: " + x.getRegion().getRegionName() + ", Country: " + x.getCountryName());
+            });
+        }
+
+        Query query2 = entityMgr.createQuery("select R  from Region R JOIN R.countries ");
+        List<Region> regionList2 = query2.getResultList();
+        for (Region r : regionList2) {
+            r.getCountries().forEach(x -> {
+                System.out.println("Region: " + x.getRegion().getRegionName() + ", Country: " + x.getCountryName());
+            });
+        }
+
+        Query query3 = entityMgr.createQuery("select r, c  from Region r JOIN r.countries c ");
+        List<Object[]> regionList3 = query3.getResultList();
+
+        regionList3.forEach(x -> {
+            System.out.println("Region: " + ((Region) x[0]).getRegionName() + ", Country: " + ((Country) x[1]).getCountryName());
+        });
+
+        Query query4 = entityMgr.createQuery("select R.regionName  from Region R ");
+        List<String> regionNameList = query4.getResultList();
+        for (String r : regionNameList) {
+            System.out.println("Regions : " + r);
+        }
+
+        //cartesian product un-related tables
+        //entityMgr.createQuery("select l, r FROM Location l, Region r");
+        //Where condition below to avoid cartesian product, where 23 locations and associated region is fetched
+        Query query5 = entityMgr.createQuery("select l, r FROM Location l, Region r WHERE l.country.region = r ");
+        List<Object[]> locationRegionList = query5.getResultList();
+        locationRegionList.forEach(x -> {
+            System.out.println("Location: [State: " + ((Location) x[0]).getState() + ", City: " + ((Location) x[0]).getCity() + "] belongs to " +
+                    "Region: " + ((Region) x[1]).getRegionName());
+        });
+
+        //LEFT JOIN, get all regions (including that don't have country, example antartica and north pole is added
+        // (total 6 regions and 25 countries which belong to 4 regions only )
+        //Opposite - "select c, r FROM Country c LEFT JOIN c.region r
+        //select r, c FROM Region r LEFT JOIN r.countries r
+        // Oracle sql = select r.*, c.* from regions r LEFT JOIN countries c ON c.region_id = r.region_id
+        //TODO - Not working due to location object
+        Query query6 = entityMgr.createQuery("select r, c FROM Region r LEFT JOIN r.countries c ");
+        List<Object[]> countryRegionList = query5.getResultList();
+
+        entityMgr.close();
+        entityMgrFactory.close();
+    }
+}
